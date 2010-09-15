14851fcd5b8e36d7eb78773742bcc65a45f251bf
strykejernene <strykejernene@gmail.com>
2010-09-15 16:07:40 +0000

    Empty implementation of the database class. Structured for simultaneous editing.

---
+++ src/OrderDB.java
@@ -0,0 +1,22 @@
+/**
+ * Class for handling of database queries
+ *
+ * @author Anders
+ */
+public class OrderDB {
+    /** Todo: empty implementations of methods needed
+     *        in the classes.
+     *
+     * PS: To avoid conflicts in simultaneous editing,
+     *     create methods under you commented name
+     */
+
+    // Anders
+
+    // Lars
+
+    // Audun
+
+    // Haavard
+    
+}
