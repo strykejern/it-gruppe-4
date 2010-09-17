0cb12363c7d760bd9db8997259af4a037b677541
strykejernene@gmail.com <strykejernene@gmail.com@97b329e5-fbd7-ce8c-d52d-c91c3dbe4029>
2010-09-17 18:58:04 +0000

    Added constructor and destructor for the database class

--- trunk/src/OrderDB.java
+++ trunk/src/OrderDB.java
@@ -1,3 +1,8 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * Class for handling of database queries
  *
@@ -8,11 +13,36 @@ public class OrderDB {
      *        in the classes.
      *
      * PS: To avoid conflicts in simultaneous editing,
-     *     create methods under you commented name
+     *     create methods under your commented name
      */
 
     // Anders
+    private Connection dbConnection;
 
+    public OrderDB(String userName, String password, String databaseLocation)
+            throws ClassNotFoundException, InstantiationException,
+            IllegalAccessException, SQLException{
+
+        Class.forName("com.mysql.jdbc.Driver").newInstance();
+        
+        dbConnection = DriverManager.getConnection(
+                databaseLocation, userName, password);
+    }
+
+    /**
+     * Close the database connection on exit
+     *
+     * If the database connection was successful,
+     * close it when class is destroyed
+     *
+     * @throws Throwable
+     */
+    @Override
+    protected void finalize() throws Throwable{
+        if (dbConnection != null){
+            dbConnection.close();
+        }
+    }
     // Lars
 		
 		/* Method for adding new dish to DB.
