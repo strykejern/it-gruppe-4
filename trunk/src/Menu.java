7347f38e35eeb30983f9d8ec92376a106fb1362b
lars.gjolme@gmail.com <lars.gjolme@gmail.com@97b329e5-fbd7-ce8c-d52d-c91c3dbe4029>
2010-09-17 17:14:13 +0000

    inneholder forelobig kun constructor av Dish (retter som skal i menyen)

---
+++ trunk/src/Menu.java
@@ -0,0 +1,22 @@
+import java.*;
+/* Class for editing menu-DB 
+ * 
+ * @author Lars
+ */
+public class Menu {
+
+}
+class Dish{
+	public int nr;			//number in menu
+	public String name;		//name of dish
+	public String contents;	//contents of dish
+
+		//creates new Dish-object
+	public Dish(int nrIn, String nameIn, String contentsIn){ 
+		this.nr=nrIn;
+		this.name=nameIn;
+		this.contents=contentsIn;
+	}
+	
+	
+}
