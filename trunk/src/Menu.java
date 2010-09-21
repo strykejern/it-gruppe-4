bfb587fc668d26b20b36c99c6b45f5f9b092a65a
lars.gjolme@gmail.com <lars.gjolme@gmail.com@97b329e5-fbd7-ce8c-d52d-c91c3dbe4029>
2010-09-21 13:52:52 +0000

    har nå metode for å lage ny rett, samt metode for å skrive en meny mangler gui-implementasjon for å ta in info om ny meny

--- trunk/src/Menu.java
+++ trunk/src/Menu.java
@@ -1,11 +1,30 @@
 import java.*;
+
 /* Class for editing menu-DB 
  * 
  * @author Lars
  */
 public class Menu {
+	//String for printing menu
+	public String menuPrint ="Nr" + "\t" + "Name" + "\t" + "\t" + "Contents" + "\n"; 
+	
+	//constructs a menu and prints it
+	public Menu(){
+		OrderDB dishGetter = new OrderDB();
+		  int nr=0;
+		  Dish nextDish = dishGetter.getDish(nr); //next dish for menubuilding
+		  while(nextDish.name!=null){
+			  menuPrint = menuPrint + nextDish.nr + "\t" + nextDish.name +"\t" + 
+			  "\t" + nextDish.contents + "\n";
+			  nr++;
+			  nextDish=dishGetter.getDish(nr);
+		  }
+		  System.out.print(this.menuPrint);
+		}
+	}
+	
+	
 
-}
 class Dish{
 	public int nr;			//number in menu
 	public String name;		//name of dish
@@ -17,12 +36,6 @@ class Dish{
 		this.name=nameIn;
 		this.contents=contentsIn;
 	}
-	class Menu{
-		public String menuPrint; //String for printing menu
-		
-		public Menu(){
-		  
-		}
-
-	}
 }
+
+
