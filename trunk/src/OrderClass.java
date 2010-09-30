8dd08cada1d6ddb0f4946e83d587edc14eda3c2a
hrdybvik@gmail.com <hrdybvik@gmail.com@97b329e5-fbd7-ce8c-d52d-c91c3dbe4029>
2010-09-30 22:40:57 +0000

    Har fylt inn noen metoder. Usikker på hva eg skal gjør, så kommentarer er veldig velkomne.

---
+++ trunk/src/OrderClass.java
@@ -0,0 +1,44 @@
+/*
+ * Bestillingsklasse
+ * Håvard
+ */
+
+public class OrderClass {
+	
+	boolean sameAddress = false;
+	String	deliveryAddres = "";
+	boolean dishIsFinished = false;
+	boolean takeAway = false;
+	
+	
+	public void takeOrder(){
+		//customerID
+		//dishNr
+		//doesCustomerExist()
+		//sameAddress
+		//comment
+		//takeAway
+	}
+	
+	public boolean doesCustomerExist(){
+		//checks if the customer already exists
+		//if false: create new customer
+		return false;
+	}
+	
+	public boolean doesDishExist(){
+		//checks if the dish ordered is valid
+		return false;
+	}
+	
+	public void deliverTo(){
+		//if(sameAddress){
+		//	deliveryAddress = Customer.address
+		//}
+	}
+	
+	
+	
+
+	
+}
