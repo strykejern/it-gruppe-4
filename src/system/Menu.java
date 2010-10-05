ae770d545132530de07d1d3e84ae7beab45e6689
lars.gjolme <lars.gjolme@gmail.com>
2010-10-05 10:36:44 +0000

    oppdatert fil med korrekt intendering

--- src/system/Menu.java
+++ src/system/Menu.java
@@ -7,37 +7,36 @@ import java.*;
  * @author Lars
  */
 public class Menu {
-	//String for printing menu
-	public String menuPrint ="Nr" + "\t" + "Name" + "\t" + "\t" + "Contents" + "\n"; 
-	
-	//constructs a menu and prints it
-	public Menu(){
-		OrderDB dishGetter = new OrderDB();
-		  int nr=0;
-		  Dish nextDish = dishGetter.getDish(nr); //next dish for menubuilding
-		  while(nextDish.name!=null){
-			  menuPrint = menuPrint + nextDish.nr + "\t" + nextDish.name +"\t" + 
-			  "\t" + nextDish.contents + "\n";
-			  nr++;
-			  nextDish=dishGetter.getDish(nr);
-		  }
-		  System.out.print(this.menuPrint);
-		}
-	}
-	
-	
+    //String for printing menu
 
-class Dish{
-	public int nr;			//number in menu
-	public String name;		//name of dish
-	public String contents;	//contents of dish
+    public String menuPrint = "Nr" + "\t" + "Name" + "\t" + "\t" + "Contents" +
+            "\n";
 
-	//creates new Dish-object
-	public Dish(int nrIn, String nameIn, String contentsIn){ 
-		this.nr=nrIn;
-		this.name=nameIn;
-		this.contents=contentsIn;
-	}
+    //constructs a menu and prints it
+    public Menu() {
+        OrderDB dishGetter = new OrderDB();
+        int nr = 0;
+        Dish nextDish = dishGetter.getDish(nr); //next dish for menubuilding
+        while (nextDish.name != null) {
+            menuPrint = menuPrint + nextDish.nr + "\t" + nextDish.name + "\t"
+                    + "\t" + nextDish.contents + "\n";
+            nr++;
+            nextDish = dishGetter.getDish(nr);
+        }
+        System.out.print(this.menuPrint);
+    }
 }
 
+class Dish {
 
+    public int nr;			//number in menu
+    public String name;		//name of dish
+    public String contents;	//contents of dish
+
+    //creates new Dish-object
+    public Dish(int nrIn, String nameIn, String contentsIn) {
+        this.nr = nrIn;
+        this.name = nameIn;
+        this.contents = contentsIn;
+    }
+}
