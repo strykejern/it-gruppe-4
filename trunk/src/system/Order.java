fa4403cfbe8001dfcbc955043860200aa76dcce0
hrdybvik@gmail.com <hrdybvik@gmail.com@97b329e5-fbd7-ce8c-d52d-c91c3dbe4029>
2010-10-13 17:31:19 +0000

    Added a bunch of constructors.
    Trenger litt hjelp for å få de til å stemme, men det kan vi ta på møtet i morgen

--- trunk/src/system/Order.java
+++ trunk/src/system/Order.java
@@ -12,25 +12,66 @@ public class Order {
 	boolean dishIsFinished = false;
 	boolean takeAway = false;
 	
-	public Order(){
-		
+	/*
+	 * Overloaded constructors taking in different parameters
+	 * Labeled as comments because of errors
+	 */
+	
+	//Takes the customer's name and the number of the dish
+	public Order(String lastName, String firstName, int nr, boolean sameAddress, boolean takeAway, String comment){
+		setDeliveryAddress();
+		//nr	= Dish.nr;
+		//lastName	= Customer.lastName;
+		//firstName = Customer.firstName;
 	}
+	
+	//Takes the customer's phone number and the number of the dish
+	public Order(int phone, int nr, boolean sameAddress, boolean takeAway, String comment){
+		setDeliveryAddress();
+		//nr	= Dish.nr;
+		//phone = Customer.phone;
+	}
+	
+	//Takes the customer's address and the number of the dish
+	public Order(String address, int nr, boolean takeAway, String comment){
+		//nr	= Dish.nr;
+		//address = Customer.address;
+	}
+	
+	//Takes the customer's name and the name of the dish
+	public Order(String lastName, String firstName, String dishName, boolean sameAddress, boolean takeAway, String comment){
+		setDeliveryAddress();
+		//dishName	= Dish.name;
+		//lastName	= Customer.lastName;
+		//firstName = Customer.firstName;
+	}
+	
+	//Takes the customer's phone number and the number of the dish
+	public Order(int phone, String dishName, boolean sameAddress, boolean takeAway, String comment){
+		setDeliveryAddress();
+		//dishName	= Dish.name;
+		//phone = Customer.phone;
+	}
+	
+	//Takes the customer's address and the number of the dish
+	public Order(String address, String dishName, boolean takeAway, String comment){
+		//dishName	= Dish.name;
+		//address = Customer.address;
+	}
+	
 	
 	public boolean setCustomer(){
 		return false;
 	}
 	
 	public boolean addDish(){
-
 		return false;
 	}
 	
-	/**public void setDeliveryAddress(){
-	*	if(sameAddress){
-	*		deliveryAddress = Customer.address;
-	*	}
-	}
-	*/
 	
-		
+	public void setDeliveryAddress(){
+		if(sameAddress){
+	//		deliveryAddress = Customer.address;
+		}
+	}
 }
