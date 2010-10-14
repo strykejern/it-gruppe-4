e9f1fb2a8502da7b49f272bd5df2e04f56d5b57a
hrdybvik <hrdybvik@gmail.com>
2010-10-14 13:55:09 +0000

    Improved order class

--- src/system/Order.java
+++ src/system/Order.java
@@ -1,4 +1,7 @@
 package system;
+
+import java.util.ArrayList;
+
 /*
  * Bestillingsklasse
  * Håvard
@@ -7,71 +10,37 @@ package system;
 public class Order {
 	
 	boolean sameAddress = false;
-	String	deliveryAddress = "";
+	String	deliveryAddress = null;
 	String	comment = "";
 	boolean dishIsFinished = false;
 	boolean takeAway = false;
 	
-	/*
-	 * Overloaded constructors taking in different parameters
-	 * Labeled as comments because of errors
-	 */
+	Object customer = null;
+	ArrayList<Dish> dishes; 
 	
-	//Takes the customer's name and the number of the dish
-	public Order(String lastName, String firstName, int nr, boolean sameAddress, boolean takeAway, String comment){
-		setDeliveryAddress();
-		//nr	= Dish.nr;
-		//lastName	= Customer.lastName;
-		//firstName = Customer.firstName;
+	public Order(){
+		dishes = new ArrayList<Dish>();
+	}
+
+	public void setCustomer(Object customer){ // TODO: Replace Object with Customer
+		// TODO: Set customer variable
 	}
 	
-	//Takes the customer's phone number and the number of the dish
-	public Order(int phone, int nr, boolean sameAddress, boolean takeAway, String comment){
-		setDeliveryAddress();
-		//nr	= Dish.nr;
-		//phone = Customer.phone;
+	public void addDish(Dish dish){
+		dishes.add(dish);
 	}
 	
-	//Takes the customer's address and the number of the dish
-	public Order(String address, int nr, boolean takeAway, String comment){
-		//nr	= Dish.nr;
-		//address = Customer.address;
+	public void setDeliveryAddress(String address){
+		deliveryAddress = address;
 	}
 	
-	//Takes the customer's name and the name of the dish
-	public Order(String lastName, String firstName, String dishName, boolean sameAddress, boolean takeAway, String comment){
-		setDeliveryAddress();
-		//dishName	= Dish.name;
-		//lastName	= Customer.lastName;
-		//firstName = Customer.firstName;
-	}
-	
-	//Takes the customer's phone number and the number of the dish
-	public Order(int phone, String dishName, boolean sameAddress, boolean takeAway, String comment){
-		setDeliveryAddress();
-		//dishName	= Dish.name;
-		//phone = Customer.phone;
-	}
-	
-	//Takes the customer's address and the number of the dish
-	public Order(String address, String dishName, boolean takeAway, String comment){
-		//dishName	= Dish.name;
-		//address = Customer.address;
-	}
-	
-	
-	public boolean setCustomer(){
-		return false;
-	}
-	
-	public boolean addDish(){
-		return false;
-	}
-	
-	
-	public void setDeliveryAddress(){
-		if(sameAddress){
-	//		deliveryAddress = Customer.address;
+	public void createOrder() throws Exception{
+		if(customer == null){
+			throw new Exception("Customer is not set");
 		}
+		if(dishes.isEmpty()){
+			throw new Exception("No dish is added");
+		}
+			// TODO: OrderDB.createOrder()
 	}
 }
