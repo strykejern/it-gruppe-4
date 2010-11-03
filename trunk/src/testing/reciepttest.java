c8e773c9f4598b9e49c7464082527b96767b9a95
lars.gjolme@gmail.com <lars.gjolme@gmail.com@97b329e5-fbd7-ce8c-d52d-c91c3dbe4029>
2010-11-03 12:45:25 +0000

    testclass for reciept

---
+++ trunk/src/testing/reciepttest.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package testing;
+import system.*;
+/**
+ *
+ * @author Lars
+ */
+public class reciepttest {
+    public static void main (String[]args){
+    Order order=new Order();
+    Customer cust = new Customer(3, "Lars", "Larsen", 9178, "Jarleveien", 
+            7089, "");
+    order.setCustomer(cust);
+    System.out.print(Reciept.) //her vil jeg hente toReciept metoden
+   }
+}
