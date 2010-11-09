1cdb3b56fe2084ed8e25d28d7d9a57e3f10f64c9
lars.gjolme@gmail.com <lars.gjolme@gmail.com@97b329e5-fbd7-ce8c-d52d-c91c3dbe4029>
2010-11-09 13:39:52 +0000

    connection with fetchedorder repaired

--- trunk/src/system/FetchedOrder.java
+++ trunk/src/system/FetchedOrder.java
@@ -11,18 +11,22 @@ public class FetchedOrder {
     private int customerId;
     private String deliveryAddress;
     public enum View{CHEF, DRIVER};
-    private View viewedBy;
+    public View viewedBy;
+    private String timeStamp = "00.00.00";
 
     private ArrayList<DishOrder> dishes;
 
-    public FetchedOrder(int orderId, int customerId, String deliveryAddress, View view) {
+    public FetchedOrder(int orderId, int customerId, String deliveryAddress,
+            View view) {
         this.orderId = orderId;
         this.customerId = customerId;
         this.deliveryAddress = deliveryAddress;
         this.viewedBy = view;
-
-
-
+        if(viewedBy == View.DRIVER){
+            if(deliveryAddress==null){
+                String getAddress = ""; //TODO create query
+            }
+        }
     }
 
     public void setDishes(ArrayList<DishOrder> dishes) {
@@ -40,9 +44,11 @@ public class FetchedOrder {
     @Override
     public String toString(){
         String FetchedOrderPrint = "Order: ";
-        switch (viewedBy){
-            case CHEF: FetchedOrderPrint+=this.orderId;
-            case DRIVER: FetchedOrderPrint+= this.orderId + " to " +
+        if(viewedBy == View.CHEF){
+            FetchedOrderPrint+=this.orderId + timeStamp ;
+            }
+        if(viewedBy == View.DRIVER){
+            FetchedOrderPrint+= this.orderId + " to " +
                     this.customerId + " at " + this.deliveryAddress;
 
 
