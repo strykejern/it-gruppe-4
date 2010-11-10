e9d80d6acfd14a149b151e27b614aa28b1cdc8a5
strykejernene@gmail.com <strykejernene@gmail.com@97b329e5-fbd7-ce8c-d52d-c91c3dbe4029>
2010-11-10 15:02:37 +0000

    source code formatting and added setDishes to Order

--- trunk/src/system/Order.java
+++ trunk/src/system/Order.java
@@ -6,45 +6,47 @@ import java.util.ArrayList;
  * Bestillingsklasse
  * H�vard
  */
-
 public class Order {
-	
-	boolean sameAddress = false;
-	String	deliveryAddress = null;
-	String	comment = "";
-	boolean dishIsFinished = false;
-	boolean takeAway = false;
-	
-	Customer customer = null;
-	ArrayList<DishOrder> dishOrder; 
-	
-	public Order(){
-		dishOrder = new ArrayList<DishOrder>();
-	}
 
-	public void setCustomer(Customer customer){ 
-		this.customer = customer;
-	}
+    boolean sameAddress = false;
+    String deliveryAddress = null;
+    String comment = "";
+    boolean dishIsFinished = false;
+    boolean takeAway = false;
+    Customer customer = null;
+    ArrayList<DishOrder> dishOrder;
 
-        public Customer getCustomer(){
-            return customer;
+    public Order() {
+        dishOrder = new ArrayList<DishOrder>();
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void addDish(DishOrder dish) {
+        dishOrder.add(dish);
+    }
+
+    public void setDishes(ArrayList<DishOrder> dishes) {
+        dishOrder = dishes;
+    }
+
+    public void setDeliveryAddress(String address) {
+        deliveryAddress = address;
+    }
+
+    public void createOrder() throws Exception {
+        if (customer == null) {
+            throw new Exception("Customer is not set");
         }
-	
-	public void addDish(DishOrder dish){
-		dishOrder.add(dish);
-	}
-	
-	public void setDeliveryAddress(String address){
-		deliveryAddress = address;
-	}
-	
-	public void createOrder() throws Exception{
-		if(customer == null){
-			throw new Exception("Customer is not set");
-		}
-		if(dishOrder.isEmpty()){
-			throw new Exception("No dish is added");
-		}
-			// TODO: OrderDB.createOrder()
-	}
+        if (dishOrder.isEmpty()) {
+            throw new Exception("No dish is added");
+        }
+        // TODO: OrderDB.createOrder()
+    }
 }
